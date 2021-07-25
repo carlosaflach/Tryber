baba648034da3a48313086534517372a2fbd77e9
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-25 18:31:02 -0300

    Validaçao do formulario com bibliotecas pronto.

--- bloco_6/dia_6.2/script.js
+++ bloco_6/dia_6.2/script.js
@@ -1,35 +1,17 @@
 import './node_modules/just-validate/dist/js/just-validate.js'
+import './DatePickerX-master/dist/js/DatePickerX.min.js'
 
   function createStateOptions() {
     const states = document.getElementById('state');
-    const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+    const stateOptions = [' ', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
     for (let index = 0; index < stateOptions.length; index += 1) {
       const createOptions = document.createElement('option');
       states.appendChild(createOptions).innerText = stateOptions[index];
       states.appendChild(createOptions).value = stateOptions[index];
     }
   }
-  
-  // const picker = new Pikaday({
-  //   field: document.getElementById('datepicker'),
-  //   format: 'D/M/YYYY',
-  //   toString(date, format) {
-  //     // you should do formatting based on the passed format,
-  //     // but we will just return 'D/M/YYYY' for simplicity
-  //     const day = date.getDate();
-  //     const month = date.getMonth() + 1;
-  //     const year = date.getFullYear();
-  //     return `${day}/${month}/${year}`;
-  //   },
-  //   parse(dateString, format) {
-  //     // dateString is the result of `toString` method
-  //     const parts = dateString.split('/');
-  //     const day = parseInt(parts[0], 10);
-  //     const month = parseInt(parts[1], 10) - 1;
-  //     const year = parseInt(parts[2], 10);
-  //     return new Date(year, month, day);
-  //   }
-  // });
+
+
   
   const clearButton = document.querySelector('.clear-button');
   function clearFields() {
@@ -44,7 +26,7 @@ import './node_modules/just-validate/dist/js/just-validate.js'
   
   clearButton.addEventListener('click', clearFields);
   
-  
+  document.getElementById('datepicker').DatePickerX.init();
   
   window.onload = function () {
     createStateOptions();

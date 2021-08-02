4bbbd27a1f5a4fe40973efedde7c7a2ceac384ca
Carlos Afonso <carlos.flach15@gmail.com>
2021-08-02 18:27:44 -0300

    Exercicios de aula.

---
+++ dia_8.4/Exemplo5_Array.reduce.js
@@ -0,0 +1,21 @@
+// Utilizando o array.filter e o reduce.
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (currentValue, number) => currentValue + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+console.log(sumNumbers(numbers)); // 152
+
+// Utilizando Somente o Array.reduce
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPair = (currentValue, number) => (
+  (number % 2 === 0) ? currentValue + number : currentValue
+);
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+console.log(sumNumbers(numbers)); // 152

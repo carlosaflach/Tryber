036de2ca6d75f763d8200530ee602a956ed0dbf8
Carlos Afonso <carlos.flach15@gmail.com>
2021-08-03 15:33:22 -0300

    Adiciona aula spread operator.

---
+++ dia_8.5/spread.ex2.js
@@ -0,0 +1,33 @@
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.7];
+
+console.log(imc(...patientInfo)); // 20.76 Aqui ele espalha as informações do array, como parametros da função.
+
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+console.log(Math.max(...randomNumbers)); // 800
+console.log(Math.min(...randomNumbers)); // 5
+
+
+// Utilização do spread em objetos.
+
+const people = {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+};
+
+const about = {
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer',
+};
+
+const customer = { ...people, ...about };
+console.log(customer); /* {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer'
+} */

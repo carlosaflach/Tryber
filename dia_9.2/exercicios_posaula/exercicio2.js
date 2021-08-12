c850ba2d1d8038f847ded4fe0e2a4fd15670464b
Carlos Afonso <carlos.flach15@gmail.com>
2021-08-12 14:42:59 -0300

    começa exercicios.

---
+++ dia_9.2/exercicios_posaula/exercicio2.js
@@ -0,0 +1,9 @@
+const fetchPromise = () => {
+  new Promise ((resolve, reject) => {
+   const myArray = Array.from(
+      {length: 10},
+      () => Math.floor(Math.random() * 50) + 1,);
+      myArray.map((element) => number * number);
+  }
+  ); 
+}

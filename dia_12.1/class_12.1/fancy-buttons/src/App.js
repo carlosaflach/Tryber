96237514e28bb6e48c6da00f973c9b9b65b87960
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-02 19:56:07 -0300

    Adiciona exercicios de fixaçao 4 e 5.

--- dia_12.1/class_12.1/fancy-buttons/src/App.js
+++ dia_12.1/class_12.1/fancy-buttons/src/App.js
@@ -1,28 +1,41 @@
- // src/App.js
- import React from 'react';
+// src/App.js
+import React from 'react';
 
- function handleButtonOne() {
-   console.log('Clicou no botão 1!');
- }
 
- function handleButtonTwo() {
-   console.log('Clicou no botão 2!');
- }
 
- function handleButtonThree() {
-   console.log('Clicou no botão 3!');
- }
+class App extends React.Component {
+  constructor() {
+    super ()
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+  }
 
- class App extends React.Component {
-   render() {
-     return (
-       <div>
-         <button onClick={ handleButtonOne }>Botão 1</button>
-         <button onClick={ handleButtonTwo }>Botão 2</button>
-         <button onClick={ handleButtonThree }>Botão 3</button>
-       </div>
-     );
-   }
- }
+  handleButtonOne() {
+    console.log('Clicou no botão 1!');
+    console.log(this);
+  }
 
- export default App;
+  handleButtonTwo() {
+    console.log('Clicou no botão 2!');
+    console.log(this);
+  }
+
+  handleButtonThree() {
+    console.log('Clicou no botão 3!');
+    console.log(this);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={ this.handleButtonOne }>Botão 1</button>
+        <button onClick={ this.handleButtonTwo }>Botão 2</button>
+        <button onClick={ this.handleButtonThree }>Botão 3</button>
+      </div>
+    );
+  }
+}
+
+export default App;

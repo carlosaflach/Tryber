ab4321aa1d71ad28ef6ef726597003b6d030e560
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-12 19:03:33 -0300

    Entendendo a requisiçao de uma api, no react com o componentDidMount.

---
+++ dia_13.1/my-interdimensional-app/src/App.js
@@ -0,0 +1,49 @@
+// App.js
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
+ fetchCharacters = async () => {
+    const requestRaw = await fetch('https://rickandmortyapi.com/api/character')
+    const resquestJson = await requestRaw.json();
+    this.setState({ characters: resquestJson.results })
+      // .then(response => response.json())
+      // .then(data => {
+      //   this.setState({ characters: data.results })
+      // })
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+            return (
+              <div className="container" key={name}>
+                <h3>{name}</h3>
+                <img src={image} alt={name} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

7052edda5f0e7b4752d6ab769f6b3152ada4e239
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-08 19:28:25 -0300

    Fourth Exercise

--- Back-End/Bloco_22/dia_22.1/exercicios_posaula/imc.js
+++ Back-End/Bloco_22/dia_22.1/exercicios_posaula/imc.js
@@ -1,6 +1,17 @@
+const readline = require('readline-sync');
+
+const readName = readline.question('How is your name?\n');
+const readAge = readline.questionInt('How old are you?\n');
+const readHeight = readline.questionFloat("What is your height?\n")
+const readWeight = readline.questionFloat("What is your weight?\n")
+
+const name = readName;
+const age = readAge;
+const weight = readWeight;
+const height = readHeight;
+
 const imc = (height, weight) => {
   return (weight / Math.pow(height, 2)).toFixed(2);
 };
 
-console.log(imc(1.73, 80));
-// module.export = imc;
+console.log(`Hello ${name}, you are ${age} years old, and your IMC is ${imc(height, weight)}`);

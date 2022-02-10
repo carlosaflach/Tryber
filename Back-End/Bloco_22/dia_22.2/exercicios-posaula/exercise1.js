a5c2a877859fcfef35684c015f32885244144dee
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-09 23:17:41 -0300

    Add files excluded

---
+++ Back-End/Bloco_22/dia_22.2/exercicios-posaula/exercise1.js
@@ -0,0 +1,24 @@
+const promiseFunc = (a, b, c) => {
+  return new Promise((resolve, reject) => {
+    if(typeof a !== 'number' || typeof b !== 'number'  || typeof c !== 'number' ) reject("Informe apenas números")
+
+    const result = ((a + b) * c);
+    if(result < 50) reject("Valor muito baixo")
+    else {
+      resolve(result);
+    }
+  })
+}
+
+
+promiseFunc(10,20,40)
+.then(resolve => console.log(resolve))
+.catch(error => console.log(error))
+
+promiseFunc(1,20,1)
+.then(resolve => console.log(resolve))
+.catch(error => console.log(error))
+
+promiseFunc(10,'s',40)
+.then(resolve => console.log(resolve))
+.catch(error => console.log(error))
---
+++ Back-End/Bloco_22/dia_22.2/io-local/readFile.js
@@ -0,0 +1,27 @@
+// const fs = require('fs');
+
+// const nomeDoArquivo = 'meu-arquivo.txt';
+
+// Módulo readFile com Callbacks => MENOS RECOMENDADO
+
+// fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+//     process.exit(1);
+//   }
+//   console.log(`Conteúdo do arquivo: ${data}`);
+// });
+
+// Módulo readFile com Promises => Mais recomendado!!
+const fs = require('fs').promises;
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1); // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
+  });

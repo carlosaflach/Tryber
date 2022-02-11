66e3235421daabc79b54ddf7676b9f2578f6a94f
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-11 16:03:25 -0300

    Usando o nodemon.

--- Back-End/Bloco_22/dia_22.4/hello-express/index.js
+++ Back-End/Bloco_22/dia_22.4/hello-express/index.js
@@ -1,12 +1,13 @@
+// Estrutura básica aplicação express.
 const express = require('express');
 
-const app = express(); // 1
+const app = express(); // 1 - Inicializa a aplicação Express
 
 app.get('/hello', handleHelloWorldRequest); // 2
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-}); // 3
+}); // 3 - Fazendo o Bind. passando a porta 3001 do localhost e deixar disponível para ser a URL da nossa aplicação.
 
 function handleHelloWorldRequest(req, res) {
   res.status(200).send('Hello World!'); // 4

41dd92448d7f0d0f3b997341521ab143e8dea78b
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-14 15:26:31 -0300

    Cria middleware para validar nome e preço, antes de criar um novo produto, ou antes de buscar um produto.

--- Back-End/Bloco_22/dia_22.5/index.js
+++ Back-End/Bloco_22/dia_22.5/index.js
@@ -14,6 +14,38 @@ app.get('/recipes', function (req, res) {
   res.status(200).json(recipes);
 });
 
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'});
+
+  next();
+};
+
+function validatePrice(req, res, next) {
+  const { price } = req.body;
+  if(!price || price < 0 || typeof price !== 'number') return res.status(400).json({ message: 'Invalid price!'});
+  next();
+}
+
+app.post('/recipes', validateName, validatePrice, function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.put('/recipes/:id', validateName, validatePrice, function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipesIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipesIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipesIndex] = { ...recipes[recipesIndex], name, price };
+
+  res.status(204).end();
+});
+
 app.get('/recipes/search', function (req, res) {
   const { name, maxPrice } = req.query;
   const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice));

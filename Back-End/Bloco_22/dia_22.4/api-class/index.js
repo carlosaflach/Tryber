69cad4ebfb61332d29d05720c5a7fa97b55e77d3
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 22:50:52 -0300

    Exercicios de fixaçao aula: Criando API com Express.

--- Back-End/Bloco_22/dia_22.4/api-class/index.js
+++ Back-End/Bloco_22/dia_22.4/api-class/index.js
@@ -8,8 +8,25 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Agua Mineral 500 ml', price: 5.0 },
+];
+
 app.get('/recipes', function (req, res) {
-  res.json(recipes);
+  res.json(recipes.sort((a, b) => {
+    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+  }));
+});
+
+app.get('/drinks', function (req, res) {
+  res.json(drinks.sort((a,b) => {
+    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+  }));
 });
 
 app.listen(3001, () => {

224738598faae7a5a0202c98885c00c224c700b0
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 23:25:31 -0300

    Adicionar filtros para query string.

--- Back-End/Bloco_22/dia_22.4/api-class/index.js
+++ Back-End/Bloco_22/dia_22.4/api-class/index.js
@@ -23,11 +23,11 @@ app.get('/recipes', function (req, res) {
   }));
 });
 
-app.get('/drinks', function (req, res) {
-  res.json(drinks.sort((a,b) => {
-    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
-  }));
-});
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price >= parseInt(minPrice));
+  res.status(200).json(filteredRecipes);
+})
 
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
@@ -37,6 +37,21 @@ app.get('/recipes/:id', function (req, res) {
 
   res.status(200).json(recipe);
 });
+
+
+app.get('/drinks', function (req, res) {
+  res.json(drinks.sort((a,b) => {
+    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+  }));
+});
+
+// Notar que é case sensitive. Ou seja, diferencia Maiúsculas de minusculas.
+app.get('/drinks/search', function (req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((drink) => drink.name.includes(name));
+  res.status(200).json(filteredDrinks);
+})
+
 
 app.get('/drinks/:id', function (req, res) {
   const { id } = req.params;

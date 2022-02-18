b512ed0e749b8b17e02eaae3104ab5f268955cc1
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-18 14:23:22 -0300

    Adiciona funçao no model para criar novo author e validar os dados de criaçao do autor.

--- Back-End/Bloco_23/dia_23.1/model-example-mysql/index.js
+++ Back-End/Bloco_23/dia_23.1/model-example-mysql/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const Author = require('./models/Author');
 const Book = require('./models/Book');
 
 const app = express();
+app.use(bodyParser.json());
 const port = 3000;
 
 app.get('/authors', async (req, res) => {
@@ -19,6 +22,17 @@ app.get('/authors/:id', async (req, res) => {
   if (!author) return res.status(404).json({ message: 'Not found' });
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async(req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if(!Author.isValid(first_name,middle_name, last_name)) {
+    return res.status(400).json({message: 'Dados inválidos'});
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+  res.status(200).json({message: 'Autor criado com sucesso!'});
 });
 
 app.get('/books', async (req, res) => {

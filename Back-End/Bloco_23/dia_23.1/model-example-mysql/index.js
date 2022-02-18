99d857b2addab34637b27d600331e0b42d0b8778
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-18 11:50:40 -0300

    Cria model books para buscar por books no DB. Cria rota para buscar por livros.

--- Back-End/Bloco_23/dia_23.1/model-example-mysql/index.js
+++ Back-End/Bloco_23/dia_23.1/model-example-mysql/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 const port = 3000;
@@ -9,5 +10,23 @@ app.get('/authors', async (req, res) => {
 
   res.status(200).json(authors);
 });
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.get('/books', async (req, res) => {
+  const books = await Book.getAllBooks();
+
+  res.status(200).json(books);
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port 3000`))

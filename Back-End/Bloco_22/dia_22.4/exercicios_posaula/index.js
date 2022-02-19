ec89c4eacc44a103222d9114e6dfeb7a0e124655
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-18 21:33:41 -0300

    Cria request do tipo post, na rota /hello, exercicio 2.

--- Back-End/Bloco_22/dia_22.4/exercicios_posaula/index.js
+++ Back-End/Bloco_22/dia_22.4/exercicios_posaula/index.js
@@ -10,6 +10,13 @@ app.get('/ping', (req, res, next) => {
   return res.status(200).json({ message: 'pong'});
 });
 
+app.post('/hello', (req, res, next) => {
+  const { name } = req.body;
+  if(!name) return res.status(404).json({ message: 'O parâmetro "name" é obrigatório'});
+
+  return res.status(200).json({ message: `Hello, ${name}` });
+})
+
 app.listen(PORT, () => {
   console.log('Aplicação ouvindo na porta 3000');
 })

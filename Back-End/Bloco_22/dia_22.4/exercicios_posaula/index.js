bff11abc75742d48a43287d62d229a97ed153f87
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-18 21:37:16 -0300

    Cria request do tipo post, na rota /greetings -exercicio 3.

--- Back-End/Bloco_22/dia_22.4/exercicios_posaula/index.js
+++ Back-End/Bloco_22/dia_22.4/exercicios_posaula/index.js
@@ -17,6 +17,16 @@ app.post('/hello', (req, res, next) => {
   return res.status(200).json({ message: `Hello, ${name}` });
 })
 
+app.post('/greetings', (req, res, next) => {
+  const { name, age } = req.body;
+  if(!name) return res.status(404).json({ message: 'O parâmetro "name" é obrigatório'});
+  if(!age) return res.status(404).json({ message: 'O parâmetro "age" é obrigatório'});
+
+  if( age <= 17) return res.status(401).json({ message: "Unauthorized" });
+
+  res.status(200).json({ message: `Hello, ${name}` });
+});
+
 app.listen(PORT, () => {
   console.log('Aplicação ouvindo na porta 3000');
 })

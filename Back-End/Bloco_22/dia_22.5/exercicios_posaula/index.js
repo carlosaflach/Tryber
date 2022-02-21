bc93f6f289594b780301c1b4ceb3d3ca3a8fc6e0
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-21 15:54:44 -0300

    Tenta resolver exercicio 2

--- Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/router');
 
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/btc', router) 
 
 app.use('/user', router);
 
--- Back-End/Bloco_22/dia_22.5/exercicios_posaula/routes/router.js
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/routes/router.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+const ENDPOINTEXTERNALAPI = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
 
 const validateUsername = require('../middlewares/validateUsername');
 const validateEmail = require('../middlewares/validateEmail');
 const validatePassword = require('../middlewares/validatePassword.js');
 const gerenateRandomToken = require('../helpers/createToken');
+const { isValidToken } = require('../middlewares/validateToken');
 
 router.post('/login',validateEmail, validatePassword, (req, res) => {
   const token = gerenateRandomToken;
   return res.status(200).json({ token: token });
+});
+
+
+router.get('/price', isValidToken, async (_req, res) => {
+  const result = await axios.get(ENDPOINTEXTERNALAPI);
+
+  res.status(200).json(result.data);
 });
 
 

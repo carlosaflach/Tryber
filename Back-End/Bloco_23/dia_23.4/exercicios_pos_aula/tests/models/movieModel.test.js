560f4c8d2867b27be8e7d2f9fab35361fe321929
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-22 21:34:50 -0300

    Implementa casos de teste para o model.

--- Back-End/Bloco_23/dia_23.4/exercicios_pos_aula/tests/models/movieModel.test.js
+++ Back-End/Bloco_23/dia_23.4/exercicios_pos_aula/tests/models/movieModel.test.js
@@ -5,37 +5,58 @@ const connection = require('../../models/connection');
 const MoviesModel = require('../../models/movieModel');
 
 
-describe('Insere um novo filme no BD', () => {
-  const payloadMovie = {
-    title: 'Example Movie',
-    directedBy: 'Jane Dow',
-    releaseYear: 1999,
-  }
-
-  before( async () => {
-    const execute = [{ insertId: 1}]; // retorno esperado nesse teste
+describe('Busca apenas um filme no BD por seu ID', () => {
+  before(async () => {
+    const execute = [[]];
 
     sinon.stub(connection, 'execute').resolves(execute);
-  });
+  })
 
-  // restauramos a função 'execute' original após os testes.
-  after( async () => {
+  after(async () => {
     connection.execute.restore();
+  })
+
+  describe('quando não existe um filme com o ID informado', () => {
+    it('retorna null', async () => {
+      const response = await MoviesModel.getById();
+      expect(response).to.be.equal(null);
+    });
   });
 
-  describe('quando é inserido com sucesso', () => {
+  describe('quando existe um filme com o ID informado', () => {
+
+    before(() => {
+      sinon.stub(MoviesModel, 'getById')
+        .resolves(
+          {
+            id: 1,
+            title: 'Example Movie',
+            directedBy: 'Jane Dow',
+            releaseYear: 1999,
+          }
+        );
+    });
+
+    after(() => {
+      MoviesModel.getById.restore();
+    })
 
     it('retorna um objeto', async () => {
-      const response = await MoviesModel.create(payloadMovie);
+      const response = await MoviesModel.getById(1);
 
-      expect(response).to.be.a('object')
+      expect(response).to.be.an('object');
     });
 
-    it('tal objeto possui o "id" do novo filme inserido', async () => {
-      const response = await MoviesModel.create(payloadMovie);
+    it('o objeto não está vazio', async () => {
+      const response = await MoviesModel.getById(1);
 
-      expect(response).to.have.a.property('id')
+      expect(response).to.be.not.empty;
     });
 
+    it('tal objeto possui as propriedades: "id", "title", "releaseYear" e "directedBy"', async () => {
+      const item = await MoviesModel.getById(1);
+
+      expect(item).to.include.all.keys('id', 'title', 'releaseYear', 'directedBy');
+    });
   });
 });

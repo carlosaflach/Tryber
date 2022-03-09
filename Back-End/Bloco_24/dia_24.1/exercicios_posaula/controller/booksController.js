7f20d1b57c60e39b00ea972696e025716523b938
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-08 22:46:11 -0300

    Implementa rotas get, getbyid, e post.

--- Back-End/Bloco_24/dia_24.1/exercicios_posaula/controller/booksController.js
+++ Back-End/Bloco_24/dia_24.1/exercicios_posaula/controller/booksController.js
@@ -16,6 +16,37 @@ booksRouter.get('/', async (req, res) => {
   }
 });
 
+booksRouter.post('/', async (req, res) => {
+  try {
+    const { title, author, pageQuantity = 0 } = req.body;
+    const book = await Book.create({
+      title,
+      author,
+      pageQuantity,
+    });
+    res.status(201).json(book);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+booksRouter.get('/:id', async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const books = await Book.findByPk(id);
+    if(!books) return res.status(404).json({ message: "Books not found"});
+    res.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+
+
 
 
 module.exports = booksRouter;

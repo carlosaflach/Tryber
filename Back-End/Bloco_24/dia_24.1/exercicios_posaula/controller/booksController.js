db80c29a62c2219360961f7c1a98a693c375449f
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-08 23:02:35 -0300

    Adiciona primeiro requisito bonus.

--- Back-End/Bloco_24/dia_24.1/exercicios_posaula/controller/booksController.js
+++ Back-End/Bloco_24/dia_24.1/exercicios_posaula/controller/booksController.js
@@ -32,12 +32,13 @@ booksRouter.post('/', async (req, res) => {
   }
 });
 
+
 booksRouter.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const bookToDelete = await Book.findByPk(id);
     await bookToDelete.destroy();
-
+    
     res.status(200);
     res.json(bookToDelete);
   } catch (e) {
@@ -50,7 +51,7 @@ booksRouter.post('/:id', async (req, res) => {
   try {
     const { title, author, pageQuantity = 0 } = req.body;
     const { id } =  req.params;
-
+    
     const result = await Book.update(
       {
         title,
@@ -58,19 +59,19 @@ booksRouter.post('/:id', async (req, res) => {
         pageQuantity,
       },
       { where: { id } },
-    );
-
-    res.status(200);
-    res.json(result);
-  } catch (err) {
-    console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
-  }
-});
-
-booksRouter.get('/:id', async (req, res) => {
+      );
+      
+      res.status(200);
+      res.json(result);
+    } catch (err) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    }
+  });
+  
+  booksRouter.get('/:id', async (req, res) => {
   try {
-
+    
     const { id } = req.params;
     const books = await Book.findByPk(id);
     if(!books) return res.status(404).json({ message: "Books not found"});
@@ -80,6 +81,8 @@ booksRouter.get('/:id', async (req, res) => {
     res.status(500).json({ message: 'Algo deu errado' });
   }
 });
+
+
 
 
 

88ca49722f1335f16ce46aac453192644efbf0f0
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-11 18:21:10 -0300

    Implementa um endpoint para buscar os pacientes e seus respectivos planos.

--- Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/index.js
+++ Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/index.js
@@ -2,9 +2,14 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 
+const { getAllPatientsPlans } = require('./Controllers/patientsController');
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/patients', getAllPatientsPlans);
+
 
 const PORT = 3000;
 

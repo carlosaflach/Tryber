98e166101c07d366b64332575c9b4b76de17aa71
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-11 18:34:48 -0300

    Implementa um endpoint que busca os pacientes e as cirurgias realizadas mas exclue o doutor.

--- Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/Controllers/patientsController.js
+++ Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/Controllers/patientsController.js
@@ -26,34 +26,35 @@ Patient.findAll({
     return res.status(500).json({ message: 'Algo deu errado' });
   });
 
-// const getPatientsAndSurgeriesNoDoctor = (_req, res) =>
-// Patient.findAll({
-//   include: {
-//     model: Surgerie,
-//     as: 'surgeries',
-//     attributes: { exclude: ['doctor'] },
-//     through: { attributes: [] }
-//   }
-// })
-//   .then((listOfPatients) => {
-//     if (listOfPatients === null) {
-//       return res.status(404).send({ message: 'No patients found' });
-//     }
-
-//     return res.status(200).json(listOfPatients);
-//   })
-//   .catch(() => res.status(500).json({ message: 'Algo deu errado' }));
-
-const createPatients = (req, res) => {
-  const { fullname, plan_id } = req.body;
-  Patient.create({ fullname, plan_id })
+  
+  const createPatients = (req, res) => {
+    const { fullname, plan_id } = req.body;
+    Patient.create({ fullname, plan_id })
     .then((response) => res.status(200).json(response))
     .catch(() => res.status(500).json({ message: 'Algo deu errado' }));
-};
+  };
+  
+  const getPatientsAndSurgeriesNoDoctor = (_req, res) =>
+  Patient.findAll({
+    include: {
+      model: Surgerie,
+      as: 'surgeries',
+      attributes: { exclude: ['doctor'] },
+      through: { attributes: [] }
+    }
+  })
+    .then((listOfPatients) => {
+      if (listOfPatients === null) {
+        return res.status(404).send({ message: 'No patients found' });
+      }
+  
+      return res.status(200).json(listOfPatients);
+    })
+    .catch(() => res.status(500).json({ message: 'Algo deu errado' }));
 
 module.exports = {
   getAllPatientsPlans,
   getAllPatientsSurgeries,
   createPatients,
-  // getPatientsAndSurgeriesNoDoctor,
+  getPatientsAndSurgeriesNoDoctor,
 };
--- Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/index.js
+++ Back-End/Bloco_24/dia_24.2/exercicios_posaula/exercise-sequelize-associations/index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 
-const { getAllPatientsPlans, getAllPatientsSurgeries, createPatients } = require('./Controllers/patientsController');
+const { getAllPatientsPlans, getAllPatientsSurgeries, createPatients, getPatientsAndSurgeriesNoDoctor } = require('./Controllers/patientsController');
 const getAllPlans = require('./Controllers/planController');
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/patients', getAllPatientsPlans);
 app.post('/patients/create', createPatients);
 app.get('/patients/surgeries', getAllPatientsSurgeries);
+app.get('/patients/nodoctor', getPatientsAndSurgeriesNoDoctor)
 app.get('/plans/:id', getAllPlans);
 
 

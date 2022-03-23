ad5ce98e080b11888de053879bd158a52ee50e56
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-23 20:44:33 -0300

    Arruma a typagem dos chares.

--- Back-End/Bloco_26/dia_26.1/exercicios_posaula/area.ts
+++ Back-End/Bloco_26/dia_26.1/exercicios_posaula/area.ts
@@ -1,19 +1,10 @@
 // ./area.ts
 
-const units = ["km²", "hm²", "dam²", "m²", "dm²", "cm²", "mm²"];
+import utils from "./utils";
 
-function makeError(unity: string) {
-    throw new Error(`A unidade ${unity} não é uma unidade válida.`)
-}
+const units = ["km²", "hm²", "dam²", "m²", "dm²", "cm²", "mm²"];
 
 function convert(value: number, forUnity: string, toUnity: string): number {
 
-    if (!units.includes(forUnity)) makeError(forUnity);
-    if (!units.includes(toUnity)) makeError(toUnity);
-
-    const forIndex = units.indexOf(forUnity);
-    const toIndex = units.indexOf(toUnity);
-    const exponent = (toIndex - forIndex);
-
-    return value * Math.pow(10, exponent);
+  return utils.convert(units, value, forUnity, toUnity);
 }
--- Back-End/Bloco_26/dia_26.1/exercicios_posaula/mass.ts
+++ Back-End/Bloco_26/dia_26.1/exercicios_posaula/mass.ts
@@ -1,17 +1,10 @@
-const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
+// ./mass.ts
 
-function makeError(unity: string) {
-    throw new Error(`A unidade ${unity} não é uma unidade válida.`)
-}
+import utils from "./utils";
+
+const units = ["kg", "hg", "dag", "g", "dg", "cg", "mg"];
 
 function convert(value: number, forUnity: string, toUnity: string): number {
 
-    if (!units.includes(forUnity)) makeError(forUnity);
-    if (!units.includes(toUnity)) makeError(toUnity);
-
-    const forIndex = units.indexOf(forUnity);
-    const toIndex = units.indexOf(toUnity);
-    const exponent = (toIndex - forIndex);
-
-    return value * Math.pow(10, exponent);
+    return utils.convert(units, value, forUnity, toUnity);
 }

2768454d2ff73bdbbd0a908a8450e184629ba8bb
Carlos Afonso <carlos.flach15@gmail.com>
2022-03-28 19:00:20 -0300

    Generics com classe.

--- Back-End/Bloco_26/dia_26.2/aula_assincrona/class_examples.ts
+++ Back-End/Bloco_26/dia_26.2/aula_assincrona/class_examples.ts
@@ -277,7 +277,6 @@ let returnNumber = identity<number, string>(100, "Olá");
 let returnString = identity<string, string>("100", "Mundo");
 let returnBoolean = identity<boolean, string>(true, "Olá, Mundo!");
 
-*/
 
 // Genércs com Interfaces
 
@@ -296,3 +295,22 @@ let returnNumber = processor(100, "Olá");
 
 console.log(returnNumber);
 
+*/
+
+//Genérics com classes.
+
+class ProcessIdentity<T, U> {
+  _value: T;
+  _message: U;
+  constructor(value: T, message: U) {
+      this._value = value;
+      this._message = message;
+  }
+  getIdentity() : T {
+      console.log(this._message);
+      return this._value
+  }
+}
+
+let processor = new ProcessIdentity<number, string>(100, "Olá");
+processor.getIdentity();  // imprime "Olá" e retorna 100

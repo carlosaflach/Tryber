39ed7409cb2394cb50aeeeec8f0018a8431daac4
Carlos Afonso <carlos.flach15@gmail.com>
2022-05-23 15:32:57 -0300

    Dia 20.2.

---
+++ Back-End/Bloco_20/dia_20.2/Primeiro_SELECT.sql
@@ -0,0 +1,5 @@
+SELECT 'Carlos Afonso Flach';
+SELECT 'Carlos ' 'Afonso ' 'Flach ' 'Erechim ' '28 anos';
+SELECT 'Carlos' AS nome, 'Afonso' AS middlename, 'Flach' AS sobrenome;
+SELECT 13 * 8;
+SELECT now() AS "Data Atual";

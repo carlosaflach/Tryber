28deac2c7c17eee3737ed2bacf69c2cb9e332701
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-06 15:36:10 -0300

    Hello World with node.js

--- Back-End/Bloco_22/dia_22.1/minha-aplicacao/index.js
+++ Back-End/Bloco_22/dia_22.1/minha-aplicacao/index.js
@@ -4,3 +4,4 @@ const meuModulo = require('../meuModulo');
 console.log(meuModulo); // { funcionalidade1: [Function: funcionalidade1], funcionalidade2: [Function: funcionalidade2] }
 
 meuModulo.funcionalidade1()
+

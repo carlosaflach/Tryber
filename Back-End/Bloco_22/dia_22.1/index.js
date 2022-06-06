cb0e7770433d6825f0d1fa3833244e101f2079e8
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-06 15:16:58 -0300

    Usando módulo fs.

--- Back-End/Bloco_22/dia_22.1/index.js
+++ Back-End/Bloco_22/dia_22.1/index.js
@@ -3,3 +3,7 @@ const { brl, usdToBrl } = require('./brlValue');
 
 console.log(`Valor do dólar: ${brl}`); // Valor do dólar: 5.37
 console.log(`10 dólares em reais: ${usdToBrl(10)}`); // 10 dólares em reais: 53.7
+
+const fs = require('fs');
+
+fs.readFileSync('./meuArquivo.txt');

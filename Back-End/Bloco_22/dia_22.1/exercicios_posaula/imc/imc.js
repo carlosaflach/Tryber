4fba1a37087d83e3e4185cc34c916910f7793f32
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-07 14:42:18 -0300

    Adiciona condicionais.

--- Back-End/Bloco_22/dia_22.1/exercicios_posaula/imc/imc.js
+++ Back-End/Bloco_22/dia_22.1/exercicios_posaula/imc/imc.js
@@ -1,8 +1,41 @@
 const readline = require('readline-sync');
 
-const weight = readline.questionFloat('Qual é o seu peso? ');
-const heigth = readline.questionFloat('Qual é sua altura? ');
-const imc = (weight/(heigth * heigth)).toFixed(2);
 
-console.log(imc);
 
+const main = () => {
+  const heigth = readline.questionFloat('Qual é sua altura? ');
+  const weight = readline.questionFloat('Qual é o seu peso? ');
+  
+  const imc = (weight/(heigth * heigth)).toFixed(2);
+  console.log(imc);
+  
+  if (imc < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
+}
+
+if (imc >= 18.5 && imc < 25) {
+  console.log('Situação: Peso normal');
+  return;
+}
+
+if (imc >= 25 && imc < 30) {
+  console.log('Situação: Acima do peso (sobrepeso)');
+  return;
+}
+
+if (imc >= 30 && imc < 35) {
+  console.log('Situação: Obesidade grau I');
+  return;
+}
+
+if (imc >= 35 && imc < 40) {
+  console.log('Situação: Obesidade grau II');
+  return;
+}
+
+console.log('Situação: Obesidade graus III e IV');
+
+}
+
+main();

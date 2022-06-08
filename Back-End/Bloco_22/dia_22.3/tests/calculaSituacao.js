1b78fc2f18aeb837fcb0f79470e5fcd5918a4c47
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-08 15:39:05 -0300

    Adiciona mais testes.

--- Back-End/Bloco_22/dia_22.3/tests/calculaSituacao.js
+++ Back-End/Bloco_22/dia_22.3/tests/calculaSituacao.js
@@ -9,3 +9,19 @@ describe('Quando a média for menor que 7', () => {
     expect(resposta).to.be.equals('reprovacao');
   });
 });
+
+describe('Quando a média for maior que 7', () => {
+  it('retorna "aprovacao"', () => {
+    const resposta = calculaSituacao(9);
+
+    expect(resposta).to.be.equals('aprovacao');
+  });
+});
+
+describe('Quando a média for igual a 7', () => {
+  it('retorna "aprovacao"', () => {
+    const resposta = calculaSituacao(7);
+
+    expect(resposta).to.be.equals('aprovacao');
+  });
+});

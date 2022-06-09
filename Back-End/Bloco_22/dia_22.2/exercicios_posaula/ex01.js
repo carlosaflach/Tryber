85d3d5917f7fb814b706f5ee13be71bacd7e87b8
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-09 10:56:02 -0300

    Separa exercicios em arquivos distintos.

--- Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex01.js
+++ Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex01.js
@@ -25,24 +25,6 @@ const ex1func = (a, b, c) => {
 //   .then((resolve) => console.log(resolve))
 //   .catch((error) => console.log(error.message));
 
-  // Ex02
 
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 100 + 1);
-  }
+module.exports = ex1func;
 
-
-  function callDoMath() {
-    /* Criamos um novo array de 3 posições
-     * e utilizamos o `map` para gerar um número aleatório
-     * para cada posição do Array
-     */
-    const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber); // função que gera um array de 3 posições, e cria um array de números aleatórios.
-    console.log(randomNumbers);
-  
-    return randomNumbers;
-  }
-
-  ex1func(...callDoMath())
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err.message));

801d7a84222655323059ffb950d5b4ee79f1e4d3
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-09 11:27:25 -0300

    Exercicio 4 - Filtra o simpson pelo id.

--- Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex04.js
+++ Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex04.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 
+// a
 async function readAll() {
   const simpsonsJson = await fs.readFile('./simpsons.json', 'utf-8')
   const simpsonsParse = await JSON.parse(simpsonsJson);
@@ -9,9 +10,28 @@ async function readAll() {
   strings.forEach((string) => console.log(string));
 }
 
+// b
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+
+  const simpsons = await JSON.parse(fileContent);
+
+  const findSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+  if(!findSimpson) {
+    throw new Error('id não encontrado')
+  }
+
+  return console.log(findSimpson);
+}
+
+// c
+
 // A função main é apenas para termos um ponto de entrada centralizado para o nosso script
-function main() {
+async function main() {
   readAll();
+  await getSimpsonById(1);
+
 }
 
 main();

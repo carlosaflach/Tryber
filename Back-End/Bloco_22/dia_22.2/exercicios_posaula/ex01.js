3e5d66495aa9687919f5cd5de06065a6e14fb7d0
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-09 10:54:02 -0300

    Implementa exercicio 02.

--- Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex01.js
+++ Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex01.js
@@ -13,14 +13,36 @@ const ex1func = (a, b, c) => {
   return promise;
 }
 
-ex1func(10, 10, 10)
-  .then((resolve) => console.log(resolve))
-  .catch((error) => console.log(error.message));
+// ex1func(10, 10, 10)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
 
-ex1func(1, 1, 'a')
-  .then((resolve) => console.log(resolve))
-  .catch((error) => console.log(error.message));
+// ex1func(1, 1, 'a')
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
 
-ex1func(1, 1, 1)
-  .then((resolve) => console.log(resolve))
-  .catch((error) => console.log(error.message));
+// ex1func(1, 1, 1)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
+
+  // Ex02
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 100 + 1);
+  }
+
+
+  function callDoMath() {
+    /* Criamos um novo array de 3 posições
+     * e utilizamos o `map` para gerar um número aleatório
+     * para cada posição do Array
+     */
+    const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber); // função que gera um array de 3 posições, e cria um array de números aleatórios.
+    console.log(randomNumbers);
+  
+    return randomNumbers;
+  }
+
+  ex1func(...callDoMath())
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err.message));

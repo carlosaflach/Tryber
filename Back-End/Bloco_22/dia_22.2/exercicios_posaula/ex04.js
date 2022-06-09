e1c936b390b602757136a48f5977997fa4413210
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-09 11:48:34 -0300

    Exercicio 4 - Remove o nelson e adiciona a Maggie.

--- Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex04.js
+++ Back-End/Bloco_22/dia_22.2/exercicios_posaula/ex04.js
@@ -60,13 +60,34 @@ async function addNelsonToFamily() {
   await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily));
 }
 
+// f
+async function replaceNelson() {
+  const fileContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+
+  // Filtramos o array para remover o personagem Nelson
+  const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '8');
+
+  // Criamos um novo Array contendo a personagem Maggie
+  const Maggie = [{ id: '15', name: 'Maggie Simpson' }]
+  const simpsonsWithMaggie = simpsonsWithoutNelson
+    .concat(Maggie);
+  // com spread seria assim: 
+
+  // const simpsonsWithMaggie = [ ...simpsonsWithoutNelson, simpsonsWithMaggie ]
+
+  // Escrevemos o novo array no arquivo e retornamos a promise de escrita
+  return fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsWithMaggie));
+}
+
 // A função main é apenas para termos um ponto de entrada centralizado para o nosso script
 async function main() {
   // readAll();
   // await getSimpsonById(1);
   // getFilteredSimpsons();
   // familySimpson();
-  addNelsonToFamily();
+  // addNelsonToFamily();
+  replaceNelson();
 }
 
 main();

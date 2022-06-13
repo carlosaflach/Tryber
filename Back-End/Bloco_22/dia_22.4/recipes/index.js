c13b125a578733234fac40ad92b691786c8fd2d6
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-13 18:34:54 -0300

    Exercicio fixação: Rota get por querystring

--- Back-End/Bloco_22/dia_22.4/recipes/index.js
+++ Back-End/Bloco_22/dia_22.4/recipes/index.js
@@ -54,6 +54,12 @@ app.get('/drinksbyname', (req, res) => {
   res.json(drinksSort);
 })
 
+app.get('/recipes/search', function (req, res) {
+  const { name } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredRecipes);
+});
+
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === Number(id));

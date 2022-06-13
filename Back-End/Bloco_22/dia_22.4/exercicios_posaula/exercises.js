2f2677bd9c98ff63418751ec2a013317e9fa84b9
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-13 20:10:33 -0300

    Ex04 - Cria método PUT na rota /users/:name/:id

--- Back-End/Bloco_22/dia_22.4/exercicios_posaula/exercises.js
+++ Back-End/Bloco_22/dia_22.4/exercicios_posaula/exercises.js
@@ -25,4 +25,12 @@ app.post('/greetings', (req, res) => {
   res.status(401).json({ message: "Unauthorized" });
 });
 
+// Ex04
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade.`});
+})
+
 app.listen(3000, () => console.log('ouvindo na porta 3000!'));

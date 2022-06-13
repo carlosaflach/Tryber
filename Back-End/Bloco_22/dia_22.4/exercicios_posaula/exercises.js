839d6dd2ba4293ec127452c49eb2d054799206b7
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-13 20:07:36 -0300

    Ex03 - Cria método POST na rota /greetings.

--- Back-End/Bloco_22/dia_22.4/exercicios_posaula/exercises.js
+++ Back-End/Bloco_22/dia_22.4/exercicios_posaula/exercises.js
@@ -10,12 +10,19 @@ app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
 
 // Ex02
 app.post('/hello', (req, res) => {
+  const { name } = req.body;
+
+  return res.status(200).json({ message: `Hello, ${name}`});
+});
+
+// Ex03
+
+app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
 
   if(age > 17) return res.status(200).json({ message: `Hello, ${name}`});
 
   res.status(401).json({ message: "Unauthorized" });
-  
 });
 
 app.listen(3000, () => console.log('ouvindo na porta 3000!'));

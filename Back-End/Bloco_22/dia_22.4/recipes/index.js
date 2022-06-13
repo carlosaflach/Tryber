2a6fbb23810c30eb4afb0e0de61196941f7606e6
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-13 19:20:14 -0300

    Fixação: Adiciona rota Put para drinks e delete.

--- Back-End/Bloco_22/dia_22.4/recipes/index.js
+++ Back-End/Bloco_22/dia_22.4/recipes/index.js
@@ -94,6 +94,41 @@ app.put('/recipes/:id', function (req, res) {
   res.status(204).end();
 });
 
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === Number(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+
+  const drinkIndex = drinks.findIndex((d) => d.id === Number(id));
+
+  if(drinkIndex === -1) return res.status(404).json({ message: 'Drink not found!'});
+
+  drinks.splice(drinkIndex, 1);
+
+  res.status(204).end();
+});
+
+app.put('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const drinkIndex = drinks.findIndex((r) => r.id === Number(id));
+
+  if (drinkIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  drinks[drinkIndex] = { ...drinks[drinkIndex], name, price };
+
+  res.status(204).end();
+});
+
 app.get('/drinks/:id', (req, res) => {
   const { id } = req.params;
   const drink = drinks.find((d) => d.id === Number(id));

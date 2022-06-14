38d8f1a3a6d452d2872d13addd284036d00eef87
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-14 20:23:27 -0300

    Adiciona router para express.

--- Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
+const router = require('./routes/routes');
+
 const app = express();
 
 app.use(bodyparser.json());
 
-app.post('/sales', (req, res) => {
-  const { productName, infos } = req.body;
-
-  res.status(200).json({})
-});
-
+app.use('/sales', router);
 
 app.listen(3001, () => { console.log('Ouvindo na porta 3001'); });
---
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { producName, infos } = req.body;
+  
+});
+
+
+module.exports = router;

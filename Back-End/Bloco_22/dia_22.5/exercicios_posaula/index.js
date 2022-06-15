0dadafbeaa74882f5938c755f6ff4159943f8b84
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-14 21:01:58 -0300

    Arruma middlewares.

--- Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const router = require('./routes/routes');
-const errorMiddleware = requiere('./middlewares/error.js');
+const errorMiddleware = require('./middlewares/error.js');
 
 const app = express();
 
--- Back-End/Bloco_22/dia_22.5/exercicios_posaula/middlewares/validations.js
+++ Back-End/Bloco_22/dia_22.5/exercicios_posaula/middlewares/validations.js
@@ -3,13 +3,14 @@ const validateName = (req, res, next) => {
     const { productName } = req.body;
     
    if(!productName) {
-     const error = new Error("O campo productName é obrigatório");
-     error.statusCode = 400;
+     const err = new Error("O campo productName é obrigatório");
+     err.statusCode = 400;
      return next(err);
    }
    if(productName.length <= 4) {
-    const error = new Error("O campo productName deve ter pelo menos 4 caracteres");
-    error.statusCode = 400;
+    const err = new Error("O campo productName deve ter pelo menos 4 caracteres");
+    err.statusCode = 400;
+    return next(err);
    }
 
    next();
@@ -28,48 +29,50 @@ const testDate = (date) => {
   return true;
 }
 
-const validateSaledate = (saleDate) => {
-  if(!saleDate) {
-    const error = new Error("O campo saleDate é obrigatório" );
-    error.statusCode = 400;
-    return next(err);
-  }
-
-  if(testDate(saleDate) === false) {
-    const error = new Error("O campo saleDate não é uma data válida");
-    error.statusCode = 400;
-    return next(err);
-  }
+const validateSaledate = (saleDate) => (req, res, next) => {
+  
 };
 
-const validateWarranty = (warrantyPeriod) => {
-  if(!warrantyPeriod) {
-    const error = new Error("O campo warrantyPeriod é obrigatório");
-    error.statusCode = 400;
-    return next(err);
-  }
 
-  if(warrantyPeriod < 1 || warrantyPeriod > 3) {
-    const error = new Error("O campo warrantyPeriod precisa estar entre 1 e 3");
-    error.statusCode = 400;
-    return next(err);
-  }
-};
 
 
 const validateInfos = (req, res, next) => {
   try {
     const { infos } = req.body;
-    const { saleDate, warrantyPeriod } = infos;
     
     if(!infos) {
-      const error = new Error("O campo infos é obrigatório" );
-      error.statusCode = 400;
+      const err = new Error("O campo infos é obrigatório" );
+      err.statusCode = 400;
       return next(err);
     }
-    validateSaledate(saleDate);
 
-    validateWarranty(warrantyPeriod);
+    if(!infos.saleDate) {
+      const err = new Error("O campo saleDate é obrigatório");
+      err.statusCode = 400;
+      return next(err);
+    }
+
+    if(infos.warrantyPeriod === undefined) {
+      const err = new Error("O campo warrantyPeriod é obrigatório");
+      err.statusCode = 400;
+      return next(err);
+    }
+  
+    const { saleDate, warrantyPeriod } = infos;
+
+    const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+    if (!dateRegex.test(saleDate)) {
+      const err = new Error("O campo saleDate não é uma data válida");
+      err.statusCode = 400;
+      return next(err);
+    }
+
+  
+    if(warrantyPeriod === 0 || warrantyPeriod > 3) {
+      const err = new Error("O campo warrantyPeriod precisa estar entre 1 e 3");
+      err.statusCode = 400;
+      return next(err);
+    }
     next();
   } catch (err) {
     console.log(err);

5618f97448de51fefe58224a740bdb8fe4d008e8
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-23 23:06:35 -0300

    Fixação: Busca livro pelo id.

--- Back-End/Bloco_23/dia_23.1/model-example/models/Books.js
+++ Back-End/Bloco_23/dia_23.1/model-example/models/Books.js
@@ -24,7 +24,27 @@ const getByAuthorId = async (authorId) => {
   }));
 }
 
+const findById = async (id) => {
+	// Repare que substituímos o id por `?` na query.
+	// Depois, ao executá-la, informamos um array com o id para o método `execute`.
+	// O `mysql2` vai realizar, de forma segura, a substituição do `?` pelo id informado.
+	const query = 'SELECT id, title, author_id FROM model_example.books WHERE id = ?'
+	const [ bookData ] = await connection.execute(query, [id]);
+
+	if (bookData.length === 0) return null;
+
+	// Utilizamos [0] para buscar a primeira linha, que deve ser a única no array de resultados, pois estamos buscando por ID.
+	
+  return bookData.map(({ id, title, author_id}) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+	
+};
+
 module.exports = {
   getAllBooks,
   getByAuthorId,
+  findById
 }

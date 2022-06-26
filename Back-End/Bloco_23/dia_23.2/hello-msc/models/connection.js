8d5de3ac1c831c39f5efb81dc7bcf6c7e3d8ea97
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-26 17:48:27 -0300

    Refatorando nosso backend para implementar o padrão MSC.

---
+++ Back-End/Bloco_23/dia_23.2/hello-msc/models/connection.js
@@ -0,0 +1,13 @@
+
+// hello-msc/models/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'sua_senha',
+  database: 'model_example',
+  });
+
+module.exports = connection;

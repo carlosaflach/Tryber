8bfeb4fe1dc52bfd07c197f632fea5f48d2cbb18
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-28 16:11:54 -0300

    Configurando nossa API com arquivo index.js.

--- Back-End/Bloco_23/dia_23.4/movieAPI/tests/controllers/movieController.test.js
+++ Back-End/Bloco_23/dia_23.4/movieAPI/tests/controllers/movieController.test.js
@@ -1,9 +1,8 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 
-const MoviesController = {
-  create: () => {}
-};
+const MoviesService = require('../../services/movieService');
+const MoviesController = require('../../controllers/movieController');
 
 describe('Ao chamar o controller de create', () => {
   describe('quando o payload informado não é válido', () => {
@@ -17,7 +16,24 @@ describe('Ao chamar o controller de create', () => {
         .returns(response);
       response.send = sinon.stub()
         .returns();
-    })
+
+      /*
+      Perceba que nosso stub também simula os comportamentos do `service`,
+      dessa forma, conseguimos testar o comportamento do controller de
+      maneira isolada.
+
+      Aqui, todos os testes que requisitarem o serviço, devem receber
+      retorno `false`.
+      */  
+
+      sinon.stub(MoviesService, 'create')
+        .resolves(false);
+    });
+
+    // Restauraremos a função `create` original após os testes.
+    after(() => {
+      MoviesService.create.restore();
+    });
 
     it('é chamado o status com o código 400', async () => {
       await MoviesController.create(request, response);
@@ -48,7 +64,19 @@ describe('Ao chamar o controller de create', () => {
         .returns(response);
       response.send = sinon.stub()
         .returns();
-    })
+      
+       /*
+        Aqui, todos os testes que requisitarem o serviço, devem receber
+        retorno `true`.
+      */
+        sinon.stub(MoviesService, 'create')
+        .resolves(true);
+    });
+
+        // Restauraremos a função `create` original após os testes.
+        after(() => {
+          MoviesService.create.restore();
+        });
 
     it('é chamado o status com o código 201', async () => {
       await MoviesController.create(request, response);

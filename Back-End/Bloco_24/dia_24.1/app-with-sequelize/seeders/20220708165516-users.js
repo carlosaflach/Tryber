62f9de9b22ae8521365f196fb97ecbf4e4305805
Carlos Afonso <carlos.flach15@gmail.com>
2022-07-10 15:32:06 -0300

    Adiciona testes para o model.

--- Back-End/Bloco_24/dia_24.1/app-with-sequelize/seeders/20220708165516-users.js
+++ Back-End/Bloco_24/dia_24.1/app-with-sequelize/seeders/20220708165516-users.js
@@ -4,14 +4,14 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [
       {
-        fullName: 'Leonardo',
+        full_name: 'Leonardo',
         email: 'leo@test.com',
         // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
-        fullName: 'JEduardo',
+        full_name: 'JEduardo',
         email: 'edu@test.com',
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),

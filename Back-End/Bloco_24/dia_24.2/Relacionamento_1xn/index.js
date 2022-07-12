796ea704d7986790f826595e50ddbea4dae4b434
Carlos Afonso <carlos.flach15@gmail.com>
2022-07-11 22:07:09 -0300

    Setup Relacionamentos Sequelize.

--- Back-End/Bloco_24/dia_24.2/Relacionamento_1xn/index.js
+++ Back-End/Bloco_24/dia_24.2/Relacionamento_1xn/index.js
@@ -22,14 +22,25 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({
-        where: { id },
-        include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number'] } }],
-      });
+
+    // Eager Loading
+    // const employee = await Employee.findOne({
+    //     where: { id },
+    //     include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number'] } }],
+    //   });
+
+    const employee = await Employee.findOne({ where: { id } });
 
     if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
 
+    // Lazy loading => Damos duas funcionalidades para o mesmo link, ou seja, se tiver a queryString true, adiciona o campo address
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
+
+    // Eager loading
     return res.status(200).json(employee);
   } catch (e) {
     console.log(e.message);
---
+++ Back-End/Bloco_24/dia_24.2/Relacionamento_nxn/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { Book, User } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));

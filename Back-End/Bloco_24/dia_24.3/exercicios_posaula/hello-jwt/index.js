2198831fe537e5bf349b33574d049daa15d3c10f
Carlos Afonso <carlos.flach15@gmail.com>
2022-07-11 23:29:56 -0300

    Exercicio 4 - Top secret

--- Back-End/Bloco_24/dia_24.3/exercicios_posaula/hello-jwt/index.js
+++ Back-End/Bloco_24/dia_24.3/exercicios_posaula/hello-jwt/index.js
@@ -23,6 +23,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
 app.post('/login', controllers.login);
+app.get('/users/me', middlewares.auth, controllers.me);
+app.get(
+  '/top-secret',
+  /* Middleware que valida o JWT e cria `req.user` */
+  middlewares.auth,
+  /* Middleware que verifica se a pessoa autenticada é admin */
+  middlewares.admin,
+  /* Controller do endpoint */
+  controllers.topSecret
+);
 
 app.use(middlewares.error);
 

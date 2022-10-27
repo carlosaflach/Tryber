63c3c0351a79a47ac8f2124179e70e1253756b6b
Carlos Afonso <carlos.flach15@gmail.com>
2022-10-26 22:09:40 -0300

    Aula de manipulacao de arquivos.

--- Ciecia-Computacao/Bloco_31/dia_31.2/aula_assincrona/manipulando_json_load.py
+++ Ciecia-Computacao/Bloco_31/dia_31.2/aula_assincrona/manipulando_json_load.py
@@ -1,13 +1,4 @@
-import json  # json é um modulo que vem embutido, porém precisamos importá-lo
-
-
-with open("pokemons.json") as file:
-    content = file.read()  # leitura do arquivo
-    pokemons = json.loads(content)["results"]  # o conteúdo é transformado em estrutura python equivalente, dicionário neste caso.
-    # acessamos a chave results que é onde contém nossa lista de pokemons
-
-print(pokemons[0]["name"])  # imprime o primeiro pokemon da lista
-
+import json
 # outra  forma é usando o load
 
 # loads carrega o JSON a partir de um texto e o load carrega o json a partir de um arquivo

84a0fb7549817a4913d02b3138dbcbc36ff4fd00
Carlos Afonso <carlos.flach15@gmail.com>
2022-11-06 15:58:09 -0300

    recomeca aula.

--- Ciecia-Computacao/Bloco_32/dia_32.1/fogao.py
+++
@@ -1,6 +0,0 @@
-from eletrodomestico import Eletrodomestico
-
-
-class Fogao(Eletrodomestico):
-    def __init__(self, cor, potencia, voltagem, preco):
-        super().__init__(cor, potencia, voltagem, preco)
--- Ciecia-Computacao/Bloco_32/dia_32.1/geladeira.py
+++
@@ -1,8 +0,0 @@
-from eletrodomestico import Eletrodomestico
-
-
-class Geladeira(Eletrodomestico):
-    def __init__(self, cor, potencia, voltagem, preco, quantidade_de_portas=1):
-        super().__init__(cor, potencia, voltagem, preco)
-        # sobrescrita do método da classe mãe
-        self.quantidade_de_portas = quantidade_de_portas

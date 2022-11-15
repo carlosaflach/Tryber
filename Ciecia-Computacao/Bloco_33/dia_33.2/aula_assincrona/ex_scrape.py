80fba1dc53c6b5cce4bd2c5fc2a5337889ef147f
Carlos Afonso <carlos.flach15@gmail.com>
2022-11-15 19:12:19 -0300

    Navegando por recursos paginados

--- Ciecia-Computacao/Bloco_33/dia_33.2/aula_assincrona/ex_scrape.py
+++ Ciecia-Computacao/Bloco_33/dia_33.2/aula_assincrona/ex_scrape.py
@@ -18,9 +18,24 @@ prices = selector.css(".product_price .price_color::text").getall()
 for product in selector.css(".product_pod"):
     title = product.css("h3 a::attr(title)").get()
     price = product.css(".price_color::text").get()
-    print(title, price)
+    # print(title, price)
 
 
 # Existe uma classe next, que podemos recuperar a url através do seu elemento âncora
-next_page_url = selector.css(".next a::attr(href)").get()
-print(next_page_url)
+# next_page_url = selector.css(".next a::attr(href)").get()
+# print(next_page_url)
+
+# Define a primeira página como próxima a ter seu conteúdo recuperado
+URL_BASE = "http://books.toscrape.com/catalogue/"
+next_page_url = 'page-1.html'
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(URL_BASE + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    for product in selector.css(".product_pod"):
+        title = product.css("h3 a::attr(title)").get()
+        price = product.css(".price_color::text").get()
+        print(title, price)
+    # Descobre qual é a próxima página
+    next_page_url = selector.css(".next a::attr(href)").get()
